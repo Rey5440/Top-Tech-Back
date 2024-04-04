5dc92b05d2dec88315de5c91ae6b4240d071091a
Facundo Fernandez <HellsingSD@gmail.com>
2024-04-04 11:39:15 -0300

    Update index.js
    123123

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-require("dotenv").config();
 const server = require("./src/app.js");
-require("./src/DB/db.js");
+const { conn } = require("./src/db/db.js");
+const {
+  getDBinfo,
+} = require("./src/controllers/productsControllers/getProductsController.js");
 
 const PORT = process.env.PORT;
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-});
+conn.sync({ force: false })
+  .then(async () => {
+    await server.listen(PORT);
+    await getDBinfo();
+    console.log(`Server listening on ${PORT}`);
+  })
+  .catch((error) => {
+    console.error("Error ocurred while starting the server:", error);
+    process.exit(1); // Termina el proceso de Node.js con código de error
+  });
+
