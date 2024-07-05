468e5ce5d2d08c7819931647e6c9e17d5bff9983
F.Fernandez <hellsingsd@gmail.com>
2024-07-05 16:15:26 -0300

    algo mas

--- src/DB/models/Services.js
+++ src/DB/models/Services.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("mongoose");
+/* const { Schema, model } = require("mongoose");
 
 const CreateServices = new Schema(
   {
@@ -14,4 +14,33 @@ const CreateServices = new Schema(
 
 const Services = model("Services", CreateServices); // este nombre solo sirve para referirme al modelo (Services)
 
+module.exports = Services; */
+
+const mongoose = require('mongoose');
+
+const serviceDetailsSchema = new mongoose.Schema({
+  precio: {
+    type: Number,
+    default: null
+  },
+  image: String
+});
+
+const categorySchema = new mongoose.Schema({
+  services: {
+    type: Map,
+    of: serviceDetailsSchema
+  }
+});
+
+const servicesSchema = new mongoose.Schema({
+  services: {
+    type: Map,
+    of: categorySchema
+  }
+});
+
+const Services = mongoose.model('Services', servicesSchema);
+
 module.exports = Services;
+
